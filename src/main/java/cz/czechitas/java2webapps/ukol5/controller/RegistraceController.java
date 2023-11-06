d2f33013894dd4cd224597e7d57e0abfa09005ec
Eva Marko <valentova.eva92@gmail.com>
2023-11-05 21:15:20 -0700

    Added RegistraceForm, methods in RegistraceController and two templates: formular and shrnuti
    RegistraceForm is Java Bean with attributes with validations. Two methods were added in RegistraceController: index and form. Template formular contains form to be filled in. Template shrnuti contains summary of data which were filled in the form.
    
    Two things are not working. Firstly, there is no error message showing when the age is not between 9-14. Secondly, the summary is showing the code of a turnus but not the particular chosen dates.

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,16 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.Period;
+
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -10,8 +19,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RegistraceController {
 
   @GetMapping("/")
-  public String formular() {
-    return "formular";
+  public ModelAndView index() {
+    ModelAndView modelAndView = new ModelAndView("/formular");
+    modelAndView.addObject("form", new RegistraceForm());
+    return modelAndView;
+  }
+
+  @PostMapping("")
+  public Object form(@Valid @ModelAttribute("form") RegistraceForm form, BindingResult bindingResult) {
+    Period period = form.getDatumNarozeni().until(LocalDate.now());
+    int vek = period.getYears();
+
+    if (bindingResult.hasErrors() || vek < 9 || vek > 14) {
+      return "/formular";
+    }
+
+    ModelAndView modelAndView = new ModelAndView("/shrnuti");
+    modelAndView.addObject("jmeno", form.getJmeno()).
+            addObject("prijmeni", form.getPrijmeni()).
+            addObject("datumNarozeni", form.getDatumNarozeni()).
+            addObject("pohlavi", form.getPohlavi()).
+            addObject("turnus", form.getTurnus()).
+            addObject("email", form.getEmail()).
+            addObject("telefon", form.getTelefon());
+    return modelAndView;
   }
 
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,76 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
+public class RegistraceForm {
+    @NotBlank
+    private String jmeno;
+    @NotBlank
+    private String prijmeni;
+    @NotNull
+    private LocalDate datumNarozeni;
+    @NotBlank
+    private String pohlavi;
+    @NotBlank
+    private String turnus;
+    @Email
+    private String email;
+    private String telefon;
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public String getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(String pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
