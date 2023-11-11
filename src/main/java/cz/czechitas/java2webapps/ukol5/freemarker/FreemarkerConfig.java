51297a433025e2411f8815f96dda98f1f203cfb9
Eva Marko <valentova.eva92@gmail.com>
2023-11-10 18:29:18 -0700

    Configuration files added
    Files RecordModel, Java17ObjectWrapper a FreemarkerConfig were added in package freemarker.

---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/freemarker/FreemarkerConfig.java
@@ -0,0 +1,29 @@
+package cz.czechitas.java2webapps.ukol5.freemarker;
+
+import freemarker.template.DefaultObjectWrapperConfiguration;
+import freemarker.template.Version;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+@Configuration
+public class FreemarkerConfig implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof FreeMarkerConfigurer configurer) {
+            Java17ObjectWrapperConfiguration objectWrapperConfiguration = new Java17ObjectWrapperConfiguration(freemarker.template.Configuration.VERSION_2_3_32);
+            Java17ObjectWrapper objectWrapper = new Java17ObjectWrapper(objectWrapperConfiguration, true);
+            configurer.getConfiguration().setObjectWrapper(objectWrapper);
+        }
+        return bean;
+    }
+
+    private static class Java17ObjectWrapperConfiguration extends DefaultObjectWrapperConfiguration {
+        protected Java17ObjectWrapperConfiguration(Version incompatibleImprovements) {
+            super(incompatibleImprovements);
+            setIterableSupport(true);
+        }
+    }
+}
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/freemarker/Java17ObjectWrapper.java
@@ -0,0 +1,65 @@
+package cz.czechitas.java2webapps.ukol5.freemarker;
+
+import freemarker.ext.beans.BeansWrapperConfiguration;
+import freemarker.ext.beans.DateModel;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.DefaultObjectWrapperConfiguration;
+import freemarker.template.TemplateModel;
+import freemarker.template.TemplateModelException;
+import freemarker.template.Version;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+public class Java17ObjectWrapper extends DefaultObjectWrapper {
+    public Java17ObjectWrapper(Version incompatibleImprovements) {
+        super(incompatibleImprovements);
+    }
+
+    public Java17ObjectWrapper(BeansWrapperConfiguration bwCfg, boolean writeProtected) {
+        super(bwCfg, writeProtected);
+    }
+
+    public Java17ObjectWrapper(DefaultObjectWrapperConfiguration dowCfg, boolean writeProtected) {
+        super(dowCfg, writeProtected);
+    }
+
+    @Override
+    protected TemplateModel handleUnknownType(Object obj) throws TemplateModelException {
+        if (obj instanceof LocalDate localDate) {
+            Date date = Date.from(localDate.atStartOfDay()
+                    .atZone(ZoneId.systemDefault())
+                    .toInstant());
+            return new DateModel(date, this);
+        }
+        if (obj instanceof LocalTime localTime) {
+            Date date = Date.from(localTime.atDate(LocalDate.now())
+                    .atZone(ZoneId.systemDefault())
+                    .toInstant());
+            return new DateModel(date, this);
+        }
+        if (obj instanceof OffsetDateTime offsetDateTime) {
+            Date date = Date.from(offsetDateTime.toInstant());
+            return new DateModel(date, this);
+        }
+        if (obj instanceof ZonedDateTime zonedDateTime) {
+            Date date = Date.from(zonedDateTime.toInstant());
+            return new DateModel(date, this);
+        }
+        if (obj instanceof OffsetTime offsetTime) {
+            Date date = Date.from(offsetTime.atDate(LocalDate.now())
+                    .toInstant());
+            return new DateModel(date, this);
+        }
+        if (obj instanceof Record record) {
+            return new RecordModel(this, record);
+        }
+        return super.handleUnknownType(obj);
+    }
+
+}
